cc0431a92d7d7aac8b5bd623bdc18ab241c23042
jwalus <walusjacob@gmail.com>
2025-05-08 21:03:46 -0500

    save

---
+++ public/content/reuseable/BusinessInfo.js
@@ -0,0 +1,16 @@
+const BusinessInfo = {
+  name: "MHS",
+  address: {
+    street: "123 Main St",
+    city: "Chicago",
+    state: "IL",
+    zip: "12345",
+  },
+  contact: {
+    phone: "(123) 123-1234",
+    email: "email@email.com",
+    website: "",
+  },
+};
+
+export default BusinessInfo;
